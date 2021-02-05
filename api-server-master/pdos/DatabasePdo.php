ca7023aee74ddbd2d2f14e85c53ad8b56afa7925
sojungoh <sojung3514@gmail.com>
2021-02-05 21:52:30 +0900

    .htaccess file 생성

--- api-server-master/pdos/DatabasePdo.php
+++ api-server-master/pdos/DatabasePdo.php
@@ -7,7 +7,7 @@ function pdoSqlConnect()
         $DB_HOST = "13.124.163.139";
         $DB_NAME = "coupang_eatsdb";
         $DB_USER = "rp2remote";
-        $DB_PW = "i4abella";
+        $DB_PW = "1234#";
         $pdo = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PW);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
