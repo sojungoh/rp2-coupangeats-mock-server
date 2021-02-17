335eae7d049a2349bc328d7d499b51324fb94a12
HanInJu <haninju95@gmail.com>
2021-02-18 03:17:01 +0900

    주문한 메뉴 및 옵션 조회 API 서버반영

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require './pdos/DatabasePdo.php';
 require './pdos/UsersPdo.php';
 require './pdos/JWTPdo.php';
 require './pdos/RestaurantPdo.php';
+require './pdos/ReviewPdo.php';
 require './pdos/AddressPdo.php';
 require './pdos/CouponsPdo.php';
 require './vendor/autoload.php';
@@ -50,6 +51,12 @@ $dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $r) 
 
     $r->addRoute('GET', '/reviews/{restaurantID}', ['RestaurantController', 'reviewInfo']);
     $r->addRoute('GET', '/restaurants/{restaurantID}/reviews', ['RestaurantController', 'reviewFilter']);
+
+    /* ******************   Review   ****************** */
+    $r->addRoute('POST', '/reviews/{reviewID}/{userID}/{isHelpful}', ['ReviewController', 'isHelpfulReview']);
+    $r->addRoute('GET', '/ordered-menu/{orderID}', ['ReviewController', 'whatIsTheMenu']);
+    $r->addRoute('POST', '/reviews', ['ReviewController', 'registerReview']);
+
 
     /* ******************   Address   ****************** */
     $r->addRoute('POST', '/address', ['AddressController', 'addUserAddress']);
@@ -128,6 +135,11 @@ switch ($routeInfo[0]) {
                 $handler = $routeInfo[1][1]; 
                 $vars = $routeInfo[2];
                 require './controllers/RestaurantController.php';
+                break;
+            case 'ReviewController':
+                $handler = $routeInfo[1][1];
+                $vars = $routeInfo[2];
+                require './controllers/ReviewController.php';
                 break;
             case 'AddressController':
                 $handler = $routeInfo[1][1]; $vars = $routeInfo[2];
