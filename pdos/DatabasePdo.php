6b4357d21af64aaa07cb53be1d81b733fb9f6653
sojungoh <sojung3514@gmail.com>
2021-02-19 20:44:06 +0900

    api update

--- pdos/DatabasePdo.php
+++ pdos/DatabasePdo.php
@@ -1,20 +1,20 @@
 <?php
 
 //Soi 테스트용 DB
-function pdoSqlConnect()
-{
-    try {
-        $DB_HOST = "13.124.163.139";
-        $DB_NAME = "coupang_eatsdb";
-        $DB_USER = "rp2remote";
-        $DB_PW = "1234#";
-        $pdo = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PW);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $pdo;
-    } catch (\Exception $e) {
-        echo $e->getMessage();
-    }
-}
+// function pdoSqlConnect()
+// {
+//     try {
+//         $DB_HOST = "13.124.163.139";
+//         $DB_NAME = "coupang_eatsdb";
+//         $DB_USER = "rp2remote";
+//         $DB_PW = "1234#";
+//         $pdo = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PW);
+//         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//         return $pdo;
+//     } catch (\Exception $e) {
+//         echo $e->getMessage();
+//     }
+// }
 
 //Heather 테스트용 DB
 // function pdoSqlConnect()
@@ -33,17 +33,17 @@ function pdoSqlConnect()
 // }
 
 //RDS 개발용 DB
-// function pdoSqlConnect()
-// {
-//     try {
-//         $DB_HOST = "coupang-eatsdb.cr4fbdipsnjz.ap-northeast-2.rds.amazonaws.com";
-//         $DB_NAME = "coupangeatsdb";
-//         $DB_USER = "soi";
-//         $DB_PW = "i4abella3514";
-//         $pdo = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PW);
-//         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//         return $pdo;
-//     } catch (\Exception $e) {
-//         echo $e->getMessage();
-//     }
-// }
+function pdoSqlConnect()
+{
+    try {
+        $DB_HOST = "coupang-eatsdb.cr4fbdipsnjz.ap-northeast-2.rds.amazonaws.com";
+        $DB_NAME = "coupangeatsdb";
+        $DB_USER = "soi";
+        $DB_PW = "i4abella3514";
+        $pdo = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PW);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    } catch (\Exception $e) {
+        echo $e->getMessage();
+    }
+}
